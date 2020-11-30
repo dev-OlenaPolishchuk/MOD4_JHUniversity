bc8ea5eebeed975a8e718bf3666ae9fb24ddc5e5
Olena olishchuk <chervone_invest@ukr.net>
2020-11-30 10:06:44 +0200

    JavaScript

---
+++ SpeakGoodBye.js
@@ -0,0 +1,14 @@
+
+
+// STEP 6:
+(function (window) {
+	// STEP 7: 
+	var byeSpeaker = {};
+	var speakWord = "Good Bye";
+	// STEP 8:
+	byeSpeaker.speak = function (name) {
+	  console.log(speakWord + " " + name);
+	}
+	// STEP 9:
+	window.byeSpeaker = byeSpeaker;
+})(window);
---
+++ SpeakHello.js
@@ -0,0 +1,14 @@
+
+
+// STEP 2:
+(function (window) {
+	// STEP 3:
+	var helloSpeaker = {};
+	var speakWord = "Hello";
+	// STEP 4:
+	helloSpeaker.speak = function(name) {
+	  console.log(speakWord + " " + name);
+	}
+	// STEP 5: 
+	window.helloSpeaker = helloSpeaker;
+})(window);
